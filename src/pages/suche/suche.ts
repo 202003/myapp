9aad8d728660e74a6994e865c10ac4eec37f7106
202003 <nico.off@gmx.net>
2018-08-14 16:01:32 +0200

    fixed file search bug

---
+++ src/pages/suche/suche.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular/';
+
+@Component({
+  selector: 'page-suche',
+  templateUrl: 'suche.html'
+})
+export class SuchenPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
